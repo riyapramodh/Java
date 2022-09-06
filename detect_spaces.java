67c20f1211e38cb24f8bccfa855e7c7ab725b45b
riyapramodh <112382228+riyapramodh@users.noreply.github.com>
2022-09-06 15:46:12 +0530

    Add files via upload

---
+++ detect_spaces.java
@@ -0,0 +1,19 @@
+public class detect_spaces {
+    public static void main(String[] args) {
+        String phrase = "ALL  THAT SHINES   IS  NOT   GOLD";
+        System.out.println(phrase);
+        System.out.println(phrase.length());
+        System.out.println("double spaces is present at these indices:");
+        System.out.println(phrase.indexOf("  "));
+        System.out.println("triple spaces are present at these indices");
+        System.out.println(phrase.indexOf("   "));
+
+
+        }
+
+
+
+
+
+    }
+
