42f06ade4915f5b7be8fd734ba2127f38554b369
riyapramodh <112382228+riyapramodh@users.noreply.github.com>
2022-09-06 14:57:17 +0530

    Add files via upload

---
+++ spaces_to_underscores.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class spaces_to_underscores {
+    public static void main(String[] args) {
+        String quote = "ALL THAT SHINES IS NOT GOLD";
+        System.out.println(quote.replace(" ","_"));
+
+    }
+}
