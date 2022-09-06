d71abfeabcd972cf60a413d787270320c217f851
riyapramodh <112382228+riyapramodh@users.noreply.github.com>
2022-09-06 16:25:26 +0530

    Add files via upload

---
+++ escape_sequence.java
@@ -0,0 +1,17 @@
+public class escape_sequence {
+    public static void main(String[] args) {
+        System.out.println("PRINT THE ACTUAL LETTER:");
+        String letter = "DEAR HARRY, THIS JAVA COURSE IS NICE.";
+        System.out.println(letter);
+        System.out.println("PRINT THE EDITED LETTER:");
+        System.out.println("DEAR HARRY,\nTHIS JAVA COURSE IS NICE.");
+
+
+        }
+
+
+
+
+
+    }
+
