2f0a20e7babdcad3fed707c8bc03d2a34cd2f604
riyapramodh <112382228+riyapramodh@users.noreply.github.com>
2022-09-06 12:23:10 +0530

    Add files via upload

---
+++ basic_multiplication_table.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class basic_multiplication_table {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER A NUMBER:");
+        float a = sc.nextFloat();
+        System.out.println("ENTER THE AMOUNT OF TIMES U WANT TO PRINT a:");
+        float b = 1f;
+        System.out.println(b*a);
+        System.out.println(++b*a);
+        System.out.println(++b*a);
+        System.out.println(++b*a);
+        System.out.println(++b*a);
+        System.out.println(++b*a);
+        System.out.println(++b*a);
+        System.out.println(++b*a);
+        System.out.println(++b*a);
+        System.out.println(++b*a);
+
+
+
+    }
+}
