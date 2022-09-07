439f54b3bb1e7ab0a91b5bf1b4c1ba4fe900d4f5
riyapramodh <112382228+riyapramodh@users.noreply.github.com>
2022-09-07 12:16:38 +0530

    Add files via upload

---
+++ reverse_natural.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class reverse_natural {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER THE VALUE OF n:");
+       int n = sc.nextInt();
+        for(int i =n; i>0; i--){
+            System.out.println(i);
+        }
+
+
+    }
+}
