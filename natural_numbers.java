9bd04d236c9c380db6f5c07f9dd3953fc4ba6f98
riyapramodh <112382228+riyapramodh@users.noreply.github.com>
2022-09-07 11:09:53 +0530

    Add files via upload

---
+++ natural_numbers.java
@@ -0,0 +1,10 @@
+public class natural_numbers {
+    public static void main(String[] args) {
+
+        int a =200;
+        while(a<=300){
+            System.out.println(a);
+            a++;
+        }
+    }
+}
