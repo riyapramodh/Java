8701bc1cce910d268a577e323cb0fc29510886e1
riyapramodh <112382228+riyapramodh@users.noreply.github.com>
2022-09-08 13:10:26 +0530

    Add files via upload

---
+++ array_reverse.java
@@ -0,0 +1,11 @@
+public class array_reverse {
+    public static void main(String[] args) {
+        System.out.println("to print an array in the reverse order:");
+
+        int[] arr2 = {10, 20, 30, 40,50};
+        for(int i = arr2.length - 1; i>=0; i--){
+            System.out.println(arr2[i]);
+        }
+        
+    }
+}
