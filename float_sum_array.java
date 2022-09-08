297811d1d0922e5820f9863d41f5e5601f28db56
riyapramodh <112382228+riyapramodh@users.noreply.github.com>
2022-09-08 14:37:20 +0530

    Add files via upload

--- float_sum_array.java
+++ float_sum_array.java
@@ -1,11 +1,25 @@
 public class float_sum_array {
     public static void main(String[] args) {
         System.out.println("to print the sum of an array of float ");
-        float sum = 0;
-        float[] arr = {10.5f, 20.5f, 30.67f, 40f,50f};
-        for(int i = 0; i< arr.length; i++){
-            sum = sum+arr[i];
+
+        int[][] age = new int [2][3];
+        age[0][0] = 100;
+        age[0][1] = 110;
+        age[0][2] = 120;
+        age[1][0] = 200;
+        age[1][1] = 210;
+        age[1][2] = 220;
+        for(int i = 0; i<age.length; i++){
+            for(int j = 0; j<age[i].length;j++ ){
+                System.out.print(age[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println("");
         }
-        System.out.println(sum);
+
+
+
+
+
     }
 }
