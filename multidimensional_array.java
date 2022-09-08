f09f65b91c03726e11bbe032c63ea8ad540f9360
riyapramodh <112382228+riyapramodh@users.noreply.github.com>
2022-09-08 14:41:20 +0530

    Add files via upload

---
+++ multidimensional_array.java
@@ -0,0 +1,25 @@
+public class multidimensional_array {
+    public static void main(String[] args) {
+
+
+        int[][] age = new int [2][3];
+        age[0][0] = 100;
+        age[0][1] = 110;
+        age[0][2] = 120;
+        age[1][0] = 200;
+        age[1][1] = 210;
+        age[1][2] = 220;
+        for(int i = 0; i<age.length; i++){
+            for(int j = 0; j<age[i].length;j++ ){
+                System.out.print(age[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+
+
+
+
+
+    }
+}
