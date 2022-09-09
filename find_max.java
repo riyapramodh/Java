a460fd728aa698aa93ff2a728d437efda818d2e4
riyapramodh <112382228+riyapramodh@users.noreply.github.com>
2022-09-09 10:15:14 +0530

    Add files via upload

---
+++ find_max.java
@@ -0,0 +1,14 @@
+public class find_max {
+    public static void main(String[] args) {
+        int max = 0;
+        int[] arr = {100, 20, 35, 60, 230};
+        for (int e :arr) {
+            if(e>max){
+                max = e;
+            }
+            }
+        System.out.println(max);
+
+        }
+    }
+
