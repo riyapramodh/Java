7f88699c5ca1e0cff720e9907276533d283b3bf9
riyapramodh <112382228+riyapramodh@users.noreply.github.com>
2022-09-17 23:05:51 +0530

    Add files via upload

---
+++ overriding.java
@@ -0,0 +1,29 @@
+import org.w3c.dom.ls.LSOutput;
+
+class A{
+    int x;
+    public void meth1(){
+        System.out.println("meth1 of class A");
+    }
+    public void meth2(){
+        System.out.println("meth2 of class A");
+    }
+}
+class B{
+    public void meth2(){
+        System.out.println("meth2 of class B");
+    }
+    public void meth3(){
+        System.out.println("meth3 of class B");
+    }
+}
+
+
+public class overriding {
+    public static void main(String[] args) {
+        A a = new A();
+        B b = new B();
+        a.meth2();
+        b.meth2();
+    }
+}
