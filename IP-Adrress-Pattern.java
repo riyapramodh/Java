4259904b333d50a8f2b9125cc5f2709d4b7df563
Riya Pramodh <112382228+riyapramodh@users.noreply.github.com>
2024-03-27 15:38:51 +0530

    Create IP-Adrress-Pattern.java

---
+++ IP-Adrress-Pattern.java
@@ -0,0 +1,25 @@
+//Write a class called MyRegex which will contain a string pattern. 
+// You need to write a regular expression and assign it to the pattern such that it can be used to validate an IP address. 
+// Use the following definition of an IP address:
+// IP address is a string in the form "A.B.C.D", where the value of A, B, C, and D may range from 0 to 255. Leading zeros are allowed. The length of A, B, C, or D can't be greater than 3.
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+class Solution{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+//Write your code here
+class MyRegex{
+    String pattern = "(\\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b)";
+}
